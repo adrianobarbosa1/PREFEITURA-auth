62be0876c0802fa52d1926a553fb7c3a1f75f002
adriano barbosa <adriano.barbosa100@gmail.com>
2023-05-20 18:40:08 -0300

    feat: creat index app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,13 @@
 import express from "express";
+import "express-async-errors";
+
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/errorHandler";
+import { currentUserRouter } from "./routes/current-user";
+import { nameApiRouter } from "./routes/name-api";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 
@@ -8,6 +17,18 @@ app.use(express.json({ limit: "50mb" }));
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
+app.use(currentUserRouter);
+app.use(nameApiRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("Listening on port 3000!");
 });
